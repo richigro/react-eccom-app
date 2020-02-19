6e71b15ff72eb66aeec7947f543162f80af99762
Ricardo Guerrero <richi_gro@hotmail.com>
2020-02-18 22:47:53 -0600

    added react router dom to package.json and example for page routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.componet';
+import {Route, Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+      <Route path="/hats" component={HatsPage}/>  
+      <Route path="/" component={HomePage}/>
+        
+      </Switch>
     </div>
   );
 }
