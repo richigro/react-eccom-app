681717fe6496bce2ca81312f9e1018cc8923c6ac
Ricardo Guerrero <richi_gro@hotmail.com>
2020-02-18 19:05:42 -0600

    added pages and components directories to manage better future expasions to the project

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuItem = () => (
+    <div className="menu-item">
+        <div className="content">
+            <h1 className='title'>HATS</h1>
+            <span>Shop Now</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.componet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+import MenuItem from '../../components/menu-item/menu-item.component';
+
+const HomePage = () => {
+    return(
+    <div className="homePage">
+        <MenuItem/>
+    </div>);
+}
+
+export default HomePage;
