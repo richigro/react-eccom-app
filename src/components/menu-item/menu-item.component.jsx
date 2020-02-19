e62e451d3085640102492a1e50d74c332819f281
Ricardo Guerrero <richi_gro@hotmail.com>
2020-02-18 20:41:54 -0600

    changed file structure and added new component menu-item to display images

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './menu-item.styles.scss';
 
-const MenuItem = () => (
-    <div className="menu-item">
+const MenuItem = ({title, imageUrl, size}) => (
+    <div  
+        style={{
+            backgroundImage: `url (${imageUrl})`
+        }}
+        className={`menu-item ${size}`}
+    >
         <div className="content">
-            <h1 className='title'>HATS</h1>
+            <h1 className='title'>{title}</h1>
             <span>Shop Now</span>
         </div>
     </div>
--- src/pages/homepage/homepage.componet.jsx
+++ src/pages/homepage/homepage.componet.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import './homepage.styles.scss';
-import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => {
     return(
     <div className="homePage">
-        <MenuItem/>
+        <Directory/>
     </div>);
 }
 
