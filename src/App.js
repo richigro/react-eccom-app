63d0f2b76b372b877b2d453556c902fde995de4d
ricardo guerrero <richi_gro@hotmail.com>
2020-02-22 12:04:03 -0600

    added new header component with links to shoppage and contact page also added company logo svg, link are not yet completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.componet';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>  
         <Route exact path="/shop" component={ShopPage}/>
