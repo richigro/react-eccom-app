bc2a202c38c65a6d07b2cb06391545174c6d03f9
ricardo guerrero <richi_gro@hotmail.com>
2020-02-23 19:22:22 -0600

    added firestore and function to store each new user to firestore db if user is not already on db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -22,8 +22,33 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 export const auth = firebase.auth();
-// export const firestore = firestore.firestore();
+export const firestore = firebase.firestore();
 
 // for Google auth
 
