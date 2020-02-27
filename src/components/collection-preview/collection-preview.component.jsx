decc3869f4b72ad4df9abe1b97b11c822a77a9c6
ricardo guerrero <richi_gro@hotmail.com>
2020-02-26 19:06:02 -0600

    added new action/reducer to add new items to cart in the redux state, currently working

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({title, items}) => (
             {
             items.filter(
                 (item, idx) => idx < MAX_NUMBER_OF_ITEMS_PER_PREVIEW)
-                .map(({id, ...itemProps}) => (
-                <CollectionItem key={id} {...itemProps}/>
+                .map(item => (
+                <CollectionItem key={item.id} item={item}/>
                 ))
             }
         </div>
